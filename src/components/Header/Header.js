00306b735461345a243285f0a82e9ba36824ef40
Tee <teedia31@gmail.com>
2019-08-27 21:41:06 -0400

    Add profile nav item for authenticated users

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,13 +25,14 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      calm-frontend
+      calm
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
         { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
         { alwaysOptions }
+        { user && <Nav.Link href={`#/authors/${user._id}`}>Profile</Nav.Link>}
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
